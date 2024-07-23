a721304f863d43f9f5ea3970cb5cd09b846e4756
Dmytro Biloshytskyi <dmitriy.biloshytskiy@gmail.com>
2024-07-23 18:22:02 +0300

    Added scripts module

---
+++ scripts/run_test.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+set -x
+# Default values
+TEST_NAME="/Blob Transaction Ordering, Single Account, Dual Blob"
+EXPERIMENT="debug"
+CLIENT="nethermind-gnosis"
+SIMULATOR="ethereum/engine"
+SIMULATOR_LOG_LEVEL=3
+CLIENT_LOG_LEVEL=3
+PROXY=""
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -t|--test)
+      TEST_NAME="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -e|--exp)
+      EXPERIMENT="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -c|--client)
+      CLIENT="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -s|--simulator)
+      SIMULATOR="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -p|--proxy)
+      PROXY="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    *)    # unknown option
+      shift # past argument
+      ;;
+  esac
+done
+
+echo "Using proxy: $PROXY"  # Final verification before using the variable
+
+echo "Started test: '$TEST_NAME' and experiment: '$EXPERIMENT' with client: '$CLIENT' and simulator: '$SIMULATOR'"
+
+if [ "$PROXY" == "" ]; then
+  echo "Proxy not set"
+else
+  echo "Using proxy: $PROXY"
+  # Get mitmproxy container ID
+  CONTAINER_ID=$(docker run -d -p 7080:8080 -p 8089:8089 -p 8082:8082 -v $PWD/scripts/proxy:/home/mitmproxy/.mitmproxy mitmproxy/mitmproxy mitmweb --listen-host 0.0.0.0 --web-host 0.0.0.0 --listen-port 8089 --web-port 8082 --set ssl_insecure=true --set hardump="/home/mitmproxy/.mitmproxy/$EXPERIMENT.har")
+fi
+
+# docker cp $(docker ps --filter "ancestor=hive/clients/nethermind:latest" --format "{{.ID}}" | awk '{print $1}'):/chainspec.json nethermind_genesis2.json
+HTTP_PROXY="$PROXY" ./hive --sim "$SIMULATOR" --sim.limit "$TEST_NAME" --client "$CLIENT" --loglevel=$CLIENT_LOG_LEVEL --sim.loglevel=$SIMULATOR_LOG_LEVEL --docker.output --results-root="scripts/experiments/$EXPERIMENT/runs" --dev.addr=127.0.0.1:3000
+
+
+if [ "$PROXY" == "" ]; then
+  echo "No need to stop proxy"
+else
+  docker stop "$CONTAINER_ID"
+  mv "scripts/proxy/$EXPERIMENT.har" "scripts/experiments/$EXPERIMENT"
+fi
+
+# Get genesis
+LOG_DIR="scripts/experiments/$EXPERIMENT/runs"
+LOG_FILE=$(find "$LOG_DIR" -type f -name "*.log" | head -n 1)
+# awk '/^ -----$/ {if (flag) {flag=0} else {flag=1; next}} flag' "$LOG_FILE" > "scripts/experiments/$EXPERIMENT/genesis_unsorted.json"
+# Sorting
+# jq -S . "scripts/experiments/$EXPERIMENT/genesis_unsorted.json" > "scripts/experiments/$EXPERIMENT/genesis.json"
+# rm "scripts/experiments/$EXPERIMENT/genesis_unsorted.json"
+
+# Get show git diff
+# git diff HEAD -- 'simulators/ethereum/engine/suites/cancun/*.go' 'clients/nethermind-gnosis/' 'internal/' > "scripts/experiments/$EXPERIMENT/git_diff.txt"
+
+echo "Finished test: '$TEST_NAME' and experiment: '$EXPERIMENT'"
